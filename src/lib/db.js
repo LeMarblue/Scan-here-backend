bb7b2b9063ee830549a010c468b013705c535254
Mar0903 <cristiangm96@hotmail.com>
2020-06-02 00:15:52 -0500

    [created]  data bases

---
+++ src/lib/db.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose')
+
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME
+} = process.env
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+
+function connect () {
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+}
+
+module.exports = {
+  connect
+}
