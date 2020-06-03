2bc1518c9918f33c10c6a5ce474deb98eac97cd1
Mar0903 <cristiangm96@hotmail.com>
2020-06-02 19:24:01 -0500

    [created]model user

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    minLength: 5
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  name: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
+  registerDate: {
+    type: Date,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: ['m', 'f', 'n'],
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  rewards: {
+    type: String,
+    required: true
+  },
+  roll: {
+    type: String,
+    enum: ['user', 'admin']
+    required: true
+  }
+
+})
+
+module.exports = mongoose.model('User', userSchema)
