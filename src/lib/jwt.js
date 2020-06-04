473f84bd5e3b49e300cb59f22a8a0eba4659c0da
Mar0903 <cristiangm96@hotmail.com>
2020-06-03 20:36:11 -0500

    [created] library of jwt

---
+++ src/lib/jwt.js
@@ -0,0 +1,18 @@
+
+const jwt = require('jsonwebtoken')
+
+const secret = process.env.SECRET_WORD
+
+function sign (payload = {}) {
+  return jwt.sign(payload, secret)
+}
+
+function verify (token = '') {
+  return jwt.verify(token, secret)
+}
+
+module.exports = {
+  ...jwt,
+  sign,
+  verify
+}
